c3ae53f21c448e34008a640f0c629e06e906f15e
Okan <okanozkaya987@gmail.com>
2022-08-30 13:35:39 +0300

    test

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar-container">
       <div className="navbar-item" id="logo">
-        Jobecco
+        Jobecco Test
       </div>
       <div className="navbar-item">
         <span id="burger-menu" class="material-symbols-outlined">

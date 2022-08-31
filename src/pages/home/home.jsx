c7f9f2453a90676c1b00884e538bf4aff02cc860
Okan <okanozkaya987@gmail.com>
2022-08-31 07:41:23 +0300

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,8 @@
 import "./home.css";
 import Navbar from "../../components/navbar/navbar";
+import magnifyglass from "../../assets/magnify-glass.svg";
+import jobs from "../../assets/jobs.svg";
+import location from "../../assets/location.svg";
 
 const Home = () => {
   return (
@@ -13,7 +16,26 @@ const Home = () => {
 const UpperHome = () => {
   return (
     <div className="upper-home">
-      <div></div>
+      <h1>
+        Never stop, until you reach your <span id="red-bg">dreams</span>.
+      </h1>
+      <div className="home-job-search">
+        <div className="home-inputs">
+          <div className="search-by-title">
+            <img src={jobs} />
+            <input></input>
+          </div>
+          <div className="search-by-location">
+            <img src={location} />
+            <input></input>
+          </div>
+        </div>
+        <div className="home-search">
+          <button>
+            <img src={magnifyglass} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
